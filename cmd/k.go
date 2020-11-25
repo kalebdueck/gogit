97ebde70555383616c2dc3fa94cba859d16045b2
kaleb.dueck@gmail.com <kaleb.dueck@gmail.com>
2020-11-24 19:34:11 -0800

    gogit k now produces visualizable info

--- cmd/k.go
+++ cmd/k.go
@@ -12,20 +12,27 @@ var kCmd = &cobra.Command{
 	Use:   "k",
 	Short: "Prints Refs",
 	Run: func(cmd *cobra.Command, args []string) {
+		dot := "digraph commits {\n"
+
 		var oids []string
 		for refname, ref := range data.IterRefs() {
-			fmt.Println(fmt.Sprintf("%s: %s", refname, ref))
+			dot += fmt.Sprintf("\"%s\" [shape=note]\n", refname)
+			dot += fmt.Sprintf("\"%s\" -> \"%s\"\n", refname, ref)
 			oids = append(oids, ref)
+
 		}
 
 		for _, oid := range base.IterCommitsAndParents(oids) {
 			commit := base.GetCommit(oid)
-			fmt.Println(oid)
+			dot += fmt.Sprintf("\"%s\" [shape=box style=filled label=\"%s\"]\n", oid, oid[:10])
 			if commit.Parent != "" {
-				fmt.Printf("Parent: %s \n", commit.Parent)
-
+				dot += fmt.Sprintf("\"%s\" -> \"%s\"\n", oid, commit.Parent)
 			}
-
 		}
+
+		dot += "}"
+
+		print(dot)
+
 	},
 }
